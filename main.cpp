b3b28da2fafcf8178f81ef750ca17d160d5313c0
Matthew Martin <mattymartin.11@googlemail.com>
2016-08-19 15:59:26 +0100

    BMI Project Initial commit

--- main.cpp
+++ main.cpp
@@ -1,45 +1,111 @@
-// Listing 22-5. Testing for Palindromes
-#include <algorithm>
+// Listing 26-3. Demonstrating Floating-Point Output
 #include <iostream>
-#include <iterator>
-#include <locale>
-#include <string>
+#include <sstream>
+#include <vector>
 
-/** Determine whether @p str is a palindrome.
- * Only letter characters are tested. Spaces and punctuation don't count.
- * Empty strings are not palindromes because that's just too easy.
- * @param str the string to test
- * @return true if @p str is the same forward and backward
- */
-bool is_palindrome(std::string str)
-
+/// Print a floating-point number in three different formats.
+/// @param precision the precision to use when printing @p value
+/// @param value the floating-point number to print
+void print(int precision, float value)
 {
-    auto is_same_char = [](char a, char b)
-    {
-        auto lowercase = [](char ch){
-            return std::tolower(ch, std::locale());
-        };
-        return lowercase(a) == lowercase(b);
-    };
+    std::cout.precision(precision);
+    std::cout << std::scientific << value << '\t';
 
-    auto non_letter = [](char a){
-        return not std::isalpha(a, std::locale());
-    };
-
-    std::string::iterator end{std::remove_if(str.begin(), str.end(), non_letter)};
-    std::string rev{str.begin(), end};
-    std::reverse(rev.begin(), rev.end());
-    return not rev.empty() and std::equal(str.begin(), end, rev.begin(), is_same_char);
+    // Set the format to general.
+    std::cout.unsetf(std::ios_base::floatfield);
+    std::cout << value << '\n';
 }
 
+int checkInt(){
+    std::string input{};
+    // How to get a number.
+    int myNumber{0};
+        while (true) {
+            std::cout << "Please enter a number: ";
+            getline(std::cin, input);
+
+            // This code converts from string to number safely.
+            std::stringstream myStream(input);
+            if (myStream >> myNumber)
+                return myNumber;
+            std::cout << "Invalid number, please try again" << std::endl;
+        }
+}
+
+bool checkBool(){
+    std::string input{};
+    // How to get a number.
+    bool myNumber{0};
+    while (true) {
+        std::cout << "Please enter 1 or 0: ";
+        getline(std::cin, input);
+
+        // This code converts from string to number safely.
+        std::stringstream myStream(input);
+        if (myStream >> myNumber)
+            return myNumber;
+        std::cout << "Not a 1 (for yes) or 0 (for no), please try again" << std::endl;
+    }
+
+}
+
+std::string checkString(){
+    std::string input = "";
+
+    // How to get a string/sentence with spaces
+    std::cout << "Please enter a name (with spaces):\n>";
+    getline(std::cin, input);
+
+    std::cout << "You entered: " << input ;
+}
+
+int BMICalculator(int const pw, int const ph){
+    return pw/(ph*ph);
+}
+
+///  Function to retrieve BMI details of user
+/// @param pName stores names
+/// @param pw & ph store weight and height respectively
+/// @param ps stores sex
+/// @param pBMI stores calculated BMI
+/// @returns details to the vectors contained in main program
+void getdetails(std::vector<std::string>& pName,
+                std::vector<int>& pw, std::vector<int>& ph,
+                std::vector<char>& ps, std::vector<int>& pBMI) {
+
+
+
+
+}
+
+/// Main program.
 int main()
 {
-    std::locale::global(std::locale{""});
-    std::cin.imbue(std::locale{});
-    std::cout.imbue(std::locale{});
+    std::vector<std::string> Names;
+    std::vector<int> weight;
+    std::vector<int> height;
+    std::vector<char> sex;
+    std::vector<int> BMI;
 
-    std::string line{};
-    while (std::getline(std::cin, line))
-        if (is_palindrome(line))
-            std::cout << line << '\n';
+    // Retrieve threshold BMI
+    int ThreshBMI{0};
+    std::cout << "First enter a threshold BMI. ";
+    while (true) {
+        ThreshBMI = checkInt();
+        std::cout << "You entered: " << ThreshBMI << ", is this correct?" << std::endl;
+        if (checkBool())
+            break;
+    }
+
+    while (true) {
+        getdetails(Names, weight, height, sex, BMI, ThreshBMI);
+        std::cout << "Would you like to enter more details? ";
+        if (checkBool())
+            break;
+    }
+
+
+
+
+    print(6, 123456.789f);
 }
